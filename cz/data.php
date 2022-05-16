14aac35ab42027869689eb1d40358aee75f23142
shtalina <99807081+shtalina@users.noreply.github.com>
2022-05-16 20:46:55 +0700

    Delete data.php

--- cz/data.php
+++
@@ -1,7 +0,0 @@
-<?php
-$json_string = 'https://portfolio.tspu.edu.ru/zzz/portfolio/meyder.php?gruppa=403';
-$jsondata = file_get_contents($json_string);
-$obj = json_decode($jsondata, true);
-
-print_r($obj);
-?>
