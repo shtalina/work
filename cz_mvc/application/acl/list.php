de4f97002540506b6c44555313743929bb83efa5
shtalina <99807081+shtalina@users.noreply.github.com>
2022-06-10 12:27:25 +0700

    Delete cz_mvc directory

--- cz_mvc/application/acl/list.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-	'all' => [
-		'list_students',
-	],
-];
-?>
--- cz_mvc/public/js/connect.js
+++
@@ -1,9 +0,0 @@
-let group = document.getElementById('get-users');
-
-async function getResponse() {
-    let response = await fetch('https://portfolio.tspu.edu.ru/zzz/portfolio/meyder.php?gruppa=' + group);
-    let content = await response.json();
-
-}
-
-getResponse()
